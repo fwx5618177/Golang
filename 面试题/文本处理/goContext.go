a86686360cba68d706cd606d6a02315336f1be9d
fwx-gitrepo3-test5 <fwx5618177@gmail.com>
2020-06-28 02:37:12 +0900

    add sometest

--- 面试题/文本处理/goContext.go
+++ 面试题/文本处理/goContext.go
@@ -9,17 +9,17 @@ import (
 )
 
 func squareSum(n int, arr []string) int {
-	fmt.Println("传进来的参数，代表个数:", n)
-	fmt.Println("此时指定奇数行的值：", arr[n-1])
+	// fmt.Println("传进来的参数，代表个数:", n)
+	// fmt.Println("此时指定奇数行的值：", arr[n-1])
 	num, _ := strconv.Atoi(arr[n-1])
 
 	if n == 1 {
 		num1, _ := strconv.Atoi(arr[0])
-		fmt.Println("最后一个的平方:", num1*num1)
+		// fmt.Println("最后一个的平方:", num1*num1)
 		return num1 * num1
 	}
 	if num >= 0 {
-		fmt.Println("平方:", num*num)
+		// fmt.Println("平方:", num*num)
 		return num*num + squareSum(n-1, arr)
 	}
 
@@ -27,56 +27,72 @@ func squareSum(n int, arr []string) int {
 
 }
 
-func list(m int, testfile0 []string, count int, total int) int {
+func outPrint(i int, output []int) {
+
+	if i+1 > len(output) {
+		return
+	}
+	// fmt.Println("i", i)
+	// fmt.Println("output[i]", output[i])
+	fmt.Println(output[i])
+	i++
+	outPrint(i, output)
+	return
+}
+
+func list(m int, testfile0 []string, count int, total int) {
 	if m+1 >= len(testfile0) {
-		if count-1 == total {
-
-			return 0
-		}
-		return 1
-
+		// fmt.Println(output)
+		return
 	}
 
 	if (m+1)%2 == 0 {
 		var sum int = 0
-		fmt.Println("m:", m)
-		fmt.Println("testfile0[m+1]:", testfile0[m+1])
+		// fmt.Println("m:", m)
+		// fmt.Println("testfile0[m+1]:", testfile0[m+1])
 		pretext := regexp.MustCompile(`\s`)
 
 		text := pretext.ReplaceAllString(testfile0[m+1], `,`)
-		fmt.Println("得到的数组：", text)
-		fmt.Println("上一行,指定的数字个数:", testfile0[m])
+		// fmt.Println("得到的数组：", text)
+		// fmt.Println("上一行,指定的数字个数:", testfile0[m])
 
 		var arr []string = strings.Split(text, ",")
 		num, _ := strconv.Atoi(testfile0[m])
 		sum = squareSum(num, arr)
 
 		count++
-		fmt.Println("最后总和：", sum)
-
+		// fmt.Println("total：", sum)
+		output = append(output, sum)
+		// fmt.Println(output)
+		// outPrint(0, output)
+		// return output
 	}
 
 	m++
-	return list(m, testfile0, count, total)
-
+	list(m, testfile0, count, total)
+	// fmt.Println(output)
+	return
 }
+
+var output []int
 
 func main() {
 	var err1 error
 	var testfile0 []string
+
+	// That's the test file name
+	// this demo get data from the test.txt
 	file, err1 := ioutil.ReadFile("test.txt")
-	fmt.Println(err1)
+	if err1 != nil {
+		fmt.Println(err1)
+	}
 
 	testfile0 = strings.Split(string(file), "\r\n")
 
 	total, _ := strconv.Atoi(testfile0[0])
 	count := 0
 	m := 0
-	judge := list(m, testfile0, count, total)
-	if judge == 0 {
-		fmt.Println("successful")
-	} else {
-		fmt.Println("error")
-	}
-
+	list(m, testfile0, count, total)
+	// fmt.Println(output)
+	outPrint(0, output)
 }
